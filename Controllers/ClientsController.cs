c590d427b5b6e7444757f88c92ce4873a40e4bb9
Allan Rodrigues <allanrodriguest@hotmail.com>
2021-08-11 00:26:19 -0300

    Initial commit

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,82 @@
+﻿using ayugAPItask.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ayugAPItask.Controllers
+{
+   [Route("api/[controller]")]
+   [ApiController]
+   public class ClientsController : ControllerBase
+   {
+      private readonly IConfiguration _configuration;
+
+      public ClientsController(IConfiguration configuration)
+      {
+         _configuration = configuration;
+      }
+
+      [HttpGet]
+      public JsonResult Get()
+      {
+         string query = @"
+                        select Id,ClientName,CPF from
+                        clients
+         ";
+
+         DataTable table = new DataTable();
+         string sqlDataSource = _configuration.GetConnectionString("myConnection");
+         MySqlDataReader myReader;
+         using(MySqlConnection mycon=new MySqlConnection(sqlDataSource))
+         {
+            mycon.Open();
+            using(MySqlCommand myCommand=new MySqlCommand(query, mycon))
+            {
+               myReader = myCommand.ExecuteReader();
+               table.Load(myReader);
+
+               myReader.Close();
+               mycon.Close();
+            }
+         }
+
+         return new JsonResult(table);
+      }
+
+      [HttpPost]
+      public JsonResult Post(Client cliente)
+      {
+         string query = @"
+                        insert into clients (ClientName,CPF) values
+                                             (@Name, @Cpf);
+                        
+         ";
+
+         DataTable table = new DataTable();
+         string sqlDataSource = _configuration.GetConnectionString("myConnection");
+         MySqlDataReader myReader;
+         using (MySqlConnection mycon = new MySqlConnection(sqlDataSource))
+         {
+            mycon.Open();
+            using (MySqlCommand myCommand = new MySqlCommand(query, mycon))
+            {
+               myCommand.Parameters.AddWithValue("@Name", cliente.Name);
+               myCommand.Parameters.AddWithValue("@Cpf", cliente.Cpf);
+               myReader = myCommand.ExecuteReader();
+               table.Load(myReader);
+
+               myReader.Close();
+               mycon.Close();
+            }
+         }
+
+         return new JsonResult("Add Successfully!");
+      }
+   }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ayugAPItask.Models
+{
+   public class Client
+   {
+      public int Id { get; set; }
+
+      public string Name { get; set; }
+
+      public string Cpf { get; set; }
+   }
+}
